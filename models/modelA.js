264a6d47389933a18998bc2cfd99ab0b9a8ce236
Christine Rowland <fordo29@gmail.com>
2021-09-23 11:16:24 -0600

    Add Dinosaur Object Model

---
+++ models/modelA.js
@@ -0,0 +1,12 @@
+var DINOSAUR =
+{
+  name: "Skippy",
+  color: "green",
+  age: 24,
+  heightInFeet: 8,
+  isHungry: true,
+  eatsMeat: true,
+  diet: ["eggs", "leaves", "other dinosaurs"],
+  attributes: ["sharp teeth", "short arms", "claws", "horn"],
+};
+console.log(DINOSAUR);
