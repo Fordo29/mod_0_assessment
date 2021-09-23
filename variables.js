7daf5040c094d7794e6c7de6500f036817c512b8
Christine Rowland <fordo29@gmail.com>
2021-09-23 11:04:40 -0600

    Add variables

---
+++ variables.js
@@ -0,0 +1,17 @@
+var movie = "Top Gun";
+var isEntertaining = true;
+console.log(movie);
+movie = "Princess Bride";
+
+console.log(movie);
+
+var num4 = 4;
+var num6 = 6;
+var product = (num4 * num6);
+console.log(product);
+
+var favoriteFoods = ["pizza", "keebab", "chippy"];
+console.log(favoriteFoods);
+favoriteFoods.shift();
+console.log(favoriteFoods);
+console.log(movie.length);
