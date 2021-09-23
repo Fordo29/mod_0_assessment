70c687cfb98bd6bdd0eda0bac5e2330d1dbeb8db
Christine Rowland <fordo29@gmail.com>
2021-09-23 11:19:31 -0600

    Add Developer Object Model

---
+++ models/modelB.js
@@ -0,0 +1,12 @@
+var DEVELOPER =
+{
+  name: "John",
+  speciality: "JavaScript",
+  yearsWorked: 4,
+  weeklyHoursWorked: 40,
+  isHappy: true,
+  worksRemote: true,
+  toolsForJob: ["laptop", "external monitor", "keyboard"],
+  officesupplies: ["Paper", "Pens", "Post-it Notes", "Highlighters"],
+}
+console.log(DEVELOPER);
